cea5f337bd4462333c3134076a8a9cb395268c42
aingaRamangalahy <aingaramangalahy@gmail.com>
2020-08-14 10:50:52 +0300

    clean routes and setup controller

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,24 +1,21 @@
 const express = require('express');
+const { 
+  getBootcamp,
+  getBootcamps,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp
+} = require('../controllers/bootcamps');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.status(200).json({success: true, message :`show all bootcamps`})
-});
+router.route('/')
+  .get(getBootcamps) // all bootcamps
+  .post(createBootcamp);
 
-router.get('/:id', (req, res) => {
-  res.status(200).json({success: true, message : `get bootcamp ${req.params.id}`})
-});
+router.route('/:id')
+  .get(getBootcamp) // single bootcamp
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
 
-router.post('/', (req, res) => {
-  res.status(200).json({success: true, message :`create bootcamp`})
-});
-
-router.put('/:id', (req, res) => {
-  res.status(200).json({success: true, message :`Update bootcamp ${req.params.id}`})
-});
-
-router.delete('/:id', (req, res) => {
-  res.status(200).json({success: true, message :`Delete bootcamp ${req.params.id}`})
-});
 
 module.exports = router
