7a54bb0926d5b0ffab2c4ed1df88353272de449a
aingaRamangalahy <aingaramangalahy@gmail.com>
2020-08-15 15:47:35 +0300

    Aggregate- calculating the average course cost

--- seeder.js
+++ seeder.js
@@ -31,7 +31,7 @@ const courses = JSON.parse(
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
-    await Course.create(courses);
+    //await Course.create(courses);
     console.log('data imported...'.green.inverse);
     process.exit();
   } catch (err) {
