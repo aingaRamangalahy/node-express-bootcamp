03229d95017b536b6e7a9997f67cb05f4d8bb1eb
aingaRamangalahy <aingaramangalahy@gmail.com>
2020-08-17 17:10:50 +0300

    update/delete

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -45,7 +45,7 @@ exports.getReview = asyncHandler( async (req, res, next) => {
 });
 
 //  @desc       Add review
-//  @route      POST /api/v1//bootcamps/:bootcampId/reviews
+//  @route      POST /api/v1/bootcamps/:bootcampId/reviews
 //  @access     Private
 exports.addReview = asyncHandler( async (req, res, next) => {
   req.body.bootcamp = req.params.bootcampId;
@@ -62,5 +62,63 @@ exports.addReview = asyncHandler( async (req, res, next) => {
   res.status(201).json({
     success: true, 
     data: review
+  })
+});
+
+
+//  @desc       update review
+//  @route      PUT /api/v1/reviews/:id
+//  @access     Private
+exports.updateReview = asyncHandler( async (req, res, next) => {
+
+  let review = await Review.findById(req.params.id);
+
+  if(!review) {
+    return next(new ErrorResponse(`No review with the id of ${req.params.id}`, 404))
+  }
+
+  // Make sure review belongs to user or user is admin
+  if (
+    review.user.toString() !== req.user.id && 
+    req.user.role !== 'admin'
+  ) {
+    return next(new ErrorResponse(`Not authorized user`, 401))
+  }
+
+  review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    success: true, 
+    data: review
+  })
+});
+
+//  @desc       delete review
+//  @route      DELETE /api/v1/reviews/:id
+//  @access     Private
+exports.deleteReview = asyncHandler( async (req, res, next) => {
+
+  const review = await Review.findById(req.params.id);
+
+  if(!review) {
+    return next(new ErrorResponse(`No review with the id of ${req.params.id}`, 404))
+  }
+
+  // Make sure review belongs to user or user is admin
+  if (
+    review.user.toString() !== req.user.id && 
+    req.user.role !== 'admin'
+  ) {
+    return next(new ErrorResponse(`Not authorized user`, 401))
+  }
+
+  await review.remove();
+
+  res.status(200).json({
+    success: true, 
+    data: {}
   })
 });
--- routes/reviews.js
+++ routes/reviews.js
@@ -2,7 +2,9 @@ const express = require('express');
 const { 
   getReviews,
   getReview,
-  addReview
+  addReview,
+  updateReview,
+  deleteReview
 } = require('../controllers/reviews');
 
 const Review = require('../models/Review');
@@ -26,6 +28,16 @@ router.route('/')
   )
 router.route('/:id') 
   .get(getReview)
+  .put(
+    protect,
+    authorize('user', 'admin'),
+    updateReview
+  )
+  .delete(
+    protect,
+    authorize('user', 'admin'),
+    deleteReview
+  )
 
 
 module.exports = router;
