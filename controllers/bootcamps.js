4793551b739ca1f4a07f85f37765fbfa28ebc1d6
aingaRamangalahy <aingaramangalahy@gmail.com>
2020-08-17 10:22:54 +0300

    bootcamp ownership

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -62,15 +62,24 @@ exports.createBootcamp =asyncHandler( async (req, res, next) => {
 //  @route      PUT /api/v1/bootcamps/:id
 //  @access     Private
 exports.updateBootcamp = asyncHandler( async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators :true
-  });
+  let bootcamp = await Bootcamp.findById(req.params.id);
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Bootcamp not found with id of "${req.params.id}"`, 404)
     );
   }
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(`User "${req.params.id}" is not authorized to update this bootcamp`, 401)
+    ); 
+  }
+
+  bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
+
   res.status(200).json({
     success: true, 
     data: bootcamp
@@ -87,6 +96,14 @@ exports.deleteBootcamp = asyncHandler( async (req, res, next) => {
       new ErrorResponse(`Bootcamp not found with id of "${req.params.id}"`, 404)
     );
   }
+
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(`User "${req.params.id}" is not authorized to delete this bootcamp`, 401)
+    ); 
+  }
+
   bootcamp.remove();
   res.status(200).json({
     success: true, 
@@ -136,6 +153,14 @@ exports.bootcampPhotoUpload = asyncHandler( async (req, res, next) => {
       new ErrorResponse(`Bootcamp not found with id of "${req.params.id}"`, 404)
     );
   }
+
+   // Make sure user is bootcamp owner
+   if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(`User "${req.params.id}" is not authorized to update this bootcamp`, 401)
+    ); 
+  }
+
   if (!req.files) {
     return next(
       new ErrorResponse(`Please add the file`, 400)
