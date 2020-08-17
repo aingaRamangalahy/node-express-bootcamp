edbf63e703ca0dd906ab4d2f4f9db16a2bb1c112
aingaRamangalahy <aingaramangalahy@gmail.com>
2020-08-17 15:11:34 +0300

    update password done

--- routes/auth.js
+++ routes/auth.js
@@ -4,13 +4,17 @@ const {
  login,
  getMe,
  forgotPassword,
- resetPassword
+ resetPassword,
+ updateDetails,
+ updatePassword
 } = require('../controllers/auth');
 
 const router = express.Router();
 const { protect } = require('../middleware/auth');
 
 router.get('/me', protect, getMe);
+router.put('/updatedetails', protect, updateDetails);
+router.put('/updatepassword', protect, updatePassword);
 router.post('/register', register);
 router.post('/login', login);
 router.post('/forgotpassword', forgotPassword);
