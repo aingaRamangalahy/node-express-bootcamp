83aeeb4f3331583cd2b03ea55f886d3bcd787be9
aingaRamangalahy <aingaramangalahy@gmail.com>
2020-08-17 15:42:05 +0300

    user crud done

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -9,13 +9,13 @@ const {
   bootcampPhotoUpload
 } = require('../controllers/bootcamps');
 const Bootcamp = require('../models/Bootcamp');
-const advancedResults = require('../middleware/advancedResults');
 
 // Include other resource routers
 const courseRouter = require('./courses');
 
 const router = express.Router();
 
+const advancedResults = require('../middleware/advancedResults');
 const { protect, authorize } = require('../middleware/auth');
 
 // Re-route into other resource routers
--- server.js
+++ server.js
@@ -17,6 +17,7 @@ connectDB();
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
 const auth = require('./routes/auth');
+const users = require('./routes/users');
 
 const app = express();
 
@@ -39,6 +40,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
 app.use('/api/v1/auth', auth);
+app.use('/api/v1/users', users);
  
 //Error handler
 app.use(errorHandler);
