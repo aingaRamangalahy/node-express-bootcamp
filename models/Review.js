1e90362b4c084ca8103c965c98b7ceb539e3e303
aingaRamangalahy <aingaramangalahy@gmail.com>
2020-08-17 16:53:16 +0300

    calculate bootcamp avg rating

--- models/Review.js
+++ models/Review.js
@@ -35,6 +35,39 @@ const ReviewSchema = new mongoose.Schema({
 });
 
 // Prevent user from submitting more than one review per bootcamp
-ReviewSchema.index({bootcamp:1, user: 1}, {unique: true})
+ReviewSchema.index({bootcamp:1, user: 1}, {unique: true});
+
+// Static method to get avg rating and save
+ReviewSchema.statics.getAverageRating = async function(bootcampId) {
+  const obj = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId }
+    },
+    {
+      $group: {
+        _id: '$bootcamp',
+        averageRating: { $avg: '$rating'}
+      }
+    }
+  ]);
+
+  try {
+    await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+      averageRating: obj[0].averageRating
+    })
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// call getAverageRating after save
+ReviewSchema.post('save', function() {
+  this.constructor.getAverageRating(this.bootcamp);
+});
+
+// call getAverageRatinf after save
+ReviewSchema.pre('remove', function() {
+  this.constructor.getAverageRating(this.bootcamp);
+});
 
 module.exports = mongoose.model('Review', ReviewSchema);
